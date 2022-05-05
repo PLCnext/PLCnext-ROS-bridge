8d8569c3dfbbb98c71d0d5e71fe5015a3f99c26a
ipa-kut <kut@p52.focal>
2022-05-05 12:06:33 +0200

    add some helpful comments

--- src/phoenix_bridge.cpp
+++ src/phoenix_bridge.cpp
@@ -3,7 +3,11 @@
 
 #include <ros/ros.h>
 
+/**
+ * @brief Create bridges for each type. Parameter passed is the simple name to lookup in config/test_params.yaml
+ */
 PhoenixBridge::PhoenixBridge()
 {
-
+  odom_bridge_.init("odometry");
+  twist_bridge_.init("twist");
 }
