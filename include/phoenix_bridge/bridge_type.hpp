e1bbaa9fdef28bde96da5630488c7efb69faaac5
ipa-kut <kut@p52.focal>
2022-05-13 13:37:52 +0200

    publisher spawning

--- include/phoenix_bridge/bridge_type.hpp
+++ include/phoenix_bridge/bridge_type.hpp
@@ -6,7 +6,8 @@
 #include "phoenix_bridge/dummy_phoenix_comm.h"
 
 #include <vector>
-#include <thread>
+#include <chrono>
+#include <functional>
 
 /**
  * @brief Class to handle creating a bridge per supported ROS msg type
@@ -19,24 +20,27 @@ template <typename T>
 class BridgeType : public rclcpp::Node
 {
 public:
-  BridgeType(std::string node_name) : Node(node_name) {}
-  BridgeType(std::string node_name, const std::string param_name);
-  void getPortParams(const std::string param_name, std::vector<PortParams> &port_vector);
-  void init(const std::string param_name);
+  BridgeType(std::string node_name);
+  void getPortParams();
+  void init();
 
 private:
   std::vector<PortParams> pub_params_; /// Parameters of publishers to create
   std::vector<PortParams> sub_params_; /// Parameters of subscribers to create
-  void publisherFunction(const PortParams port);
+  void publisherFunction(typename rclcpp::Publisher<T>::SharedPtr pub, std::string datapath);
   DummyPhoenixComm<T> comm_;
-//  std::vector<ros::Subscriber> subscribers_;
+  std::vector<std::string> pub_topics_, pub_datapaths_;
+  std::vector<std::string> sub_topics_, sub_datapaths_;
+  std::vector<long int> pub_freqs_, sub_freqs_;
+  std::vector<rclcpp::TimerBase::SharedPtr> pub_timers_;
+  std::vector<typename rclcpp::Publisher<T>::SharedPtr> pubs_;
 };
 
 template<typename T> inline
-BridgeType<T>::BridgeType(std::string node_name, const std::string param_name) :
+BridgeType<T>::BridgeType(std::string node_name) :
   Node(node_name)
 {
-  init(param_name);
+  init();
 }
 
 /**
@@ -44,35 +48,30 @@ BridgeType<T>::BridgeType(std::string node_name, const std::string param_name) :
  * @param param_name The simple name for the parameters of this type ex: "odometry" or "twist"
  */
 template<typename T> inline
-void BridgeType<T>::init(const std::string param_name)
+void BridgeType<T>::init()
 {
-  this->declare_parameter("grpc.address");
-  this->declare_parameter("grpc.type");
-  this->declare_parameter("msg_type");
-  this->declare_parameter("publishers.topics");
-  this->declare_parameter("publishers.datapaths");
-  this->declare_parameter("publishers.frequencies");
-  this->declare_parameter("subscribers.topics");
-  this->declare_parameter("subscribers.datapaths");
-  this->declare_parameter("subscribers.frequencies");
 
-//  getPortParams(param_name + "/publishers", pub_params_);
-//  getPortParams(param_name + "/subscribers", sub_params_);
-//  std::string address;
-//  if(!ros::param::get("communication/grpc/address", address))
-//  {
-//    ROS_ERROR_STREAM(ros::this_node::getName() << " Could not find grpc address param");
-//  }
+  getPortParams();
 
   /// Initialise communication layer
-//  comm_.init(address);
+  comm_.init(this->get_parameter("grpc.address").as_string());
 
-//  /// Spawn pubs
-//  for (auto port:pub_params_)
-//  {
-//    std::thread thr(std::bind(&BridgeType::publisherFunction, this, port));
+  /// Spawn pubs
+  for (size_t i=0; i<pub_topics_.size(); i++)
+  {
+    RCLCPP_INFO(this->get_logger(),
+                       "Spawning publisher  [%s, %s, %d]", pub_topics_[i].c_str(), pub_datapaths_[i].c_str(), pub_freqs_[i]);
+
+    typename rclcpp::Publisher<T>::SharedPtr pub = this->create_publisher<T>(pub_topics_[i], 1000);
+    pubs_.push_back(pub);
+    std::function<void()> fcn = std::bind(&BridgeType::publisherFunction, this, pub, pub_datapaths_[i]);
+
+    pub_timers_.push_back(this->create_wall_timer(std::chrono::milliseconds(1000/pub_freqs_[i]),fcn));
+
+//    std::thread thr(std::bind(&BridgeType::publisherFunction,
+//                              this, pub_topics_[i], pub_datapaths_[i]));
 //    thr.detach(); // Thread nust ensure graceful shutdown of itself since it is detached
-//  }
+  }
 
 //  /// Spawn subs
 //  for (auto port:sub_params_)
@@ -89,33 +88,59 @@ void BridgeType<T>::init(const std::string param_name)
 }
 
 /**
- * @brief Read the ports to create for the given type as defined by rosparams, typically uploaded from config/test_params.yaml
- *        Expect to read a list of lists where the embedded list corresponds
- *          to data fields as defineed by phoenix_contact/params_struct.h
+ * @brief Read params for this node. Param names are hardcoded so the param file should have a very specific structure.
  * @param param_name The fully resolved param path ex: "/odometry/publishers"
  * @param port_vector Save all the read param data into a vector of PortParams
  */
 template<typename T> inline
-void BridgeType<T>::getPortParams(const std::string param_name, std::vector<PortParams> &port_vector)
+void BridgeType<T>::getPortParams()
 {
-  /// @todo Verify types as per https://answers.ros.org/question/318544/retrieve-list-of-lists-from-yaml-file-parameter-server/
   /// @todo Try catch everything
-//  XmlRpc::XmlRpcValue param_list;
-//  if(!nh_.getParam(param_name, param_list))
-//  {
-//    ROS_ERROR_STREAM(ros::this_node::getName() << " Could not find " << param_name);
-//  }
-//  for(int i=0; i<param_list.size(); i++)
-//  {
-//    /// 0 - topic_name - std::string
-//    /// 1 - datapath (i.e. instance path for gRPC - std::string
-//    /// 2 - frequency - int
-//    port_vector.push_back(PortParams(param_list[i][0], param_list[i][1], param_list[i][2]));
-//  }
-//  for (auto ele:port_vector)
-//  {
-//    ROS_INFO_STREAM(param_name << ": " << ele.name_ << " " << ele.datapath_ << " " << ele.frequency_);
-//  }
+  this->declare_parameter("grpc.address");
+  this->declare_parameter("grpc.type");
+  this->declare_parameter("msg_type");
+  this->declare_parameter("publishers.topics");
+  this->declare_parameter("publishers.datapaths");
+  this->declare_parameter("publishers.frequencies");
+  this->declare_parameter("subscribers.topics");
+  this->declare_parameter("subscribers.datapaths");
+  this->declare_parameter("subscribers.frequencies");
+
+  pub_topics_ = this->get_parameter("publishers.topics").as_string_array();
+  pub_datapaths_ = this->get_parameter("publishers.datapaths").as_string_array();
+  pub_freqs_ = this->get_parameter("publishers.frequencies").as_integer_array();
+
+  sub_topics_ = this->get_parameter("subscribers.topics").as_string_array();
+  sub_datapaths_ = this->get_parameter("subscribers.datapaths").as_string_array();
+  sub_freqs_ = this->get_parameter("subscribers.frequencies").as_integer_array();
+
+  if (!((pub_topics_.size() == pub_datapaths_.size())
+         && (pub_topics_.size() == pub_freqs_.size())))
+  {
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Publisher array sizes not equal!!");
+    rclcpp::shutdown();
+  }
+  if (!((sub_topics_.size() == sub_datapaths_.size())
+         && (sub_topics_.size() == sub_freqs_.size())))
+  {
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Subscriber array sizes not equal!!");
+    rclcpp::shutdown();
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Publishers: ");
+  for (size_t i=0; i<pub_topics_.size(); i++)
+  {
+    RCLCPP_INFO_STREAM(this->get_logger(),
+                       "  [" << pub_topics_[i] << ", " << pub_datapaths_[i] << ", " << pub_freqs_[i] << "]" );
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Subscribers: ");
+  for (size_t i=0; i<sub_topics_.size(); i++)
+  {
+    RCLCPP_INFO_STREAM(this->get_logger(),
+                       "  [" << sub_topics_[i] << ", " << sub_datapaths_[i] << ", " << sub_freqs_[i] << "]" );
+  }
+  RCLCPP_INFO(this->get_logger(), "");
 }
 
 /**
@@ -123,17 +148,12 @@ void BridgeType<T>::getPortParams(const std::string param_name, std::vector<Port
  * @param port PortParam object, specifying which datapath to query data for, which topic to publish on and at what frequency
  */
 template<typename T> inline
-void BridgeType<T>::publisherFunction(const PortParams port)
+void BridgeType<T>::publisherFunction(typename rclcpp::Publisher<T>::SharedPtr pub, std::string datapath)
 {
-//  ros::Publisher pub = nh_.advertise<T>(port.name_, 1000); /// @todo Parametrise the publisher buffer size? Pros/Cons?
-//  while(ros::ok())
-//  {
-//    ros::spinOnce();
-//    T msg_rcvd;
-//    comm_.getFromPLC(port.datapath_, msg_rcvd);
-//    pub.publish(boost::make_shared<T>(msg_rcvd)); /// 0 copy transfer
-//    ros::Rate(port.frequency_).sleep();
-//  }
+  T msg_rcvd;
+  comm_.getFromPLC(datapath, msg_rcvd);
+  typename T::SharedPtr msg_ptr = std::make_shared<T>(msg_rcvd);
+  pub->publish(msg_rcvd); // is this 0 copy transfer???
 }
 
 #endif // BRIDGE_TYPE_H
