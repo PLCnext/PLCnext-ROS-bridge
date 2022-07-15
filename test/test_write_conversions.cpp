88439f9b0e9aa448397feb2b6debd84e3a721f7f
kut-docker <kut-docker@ubuntu.20>
2022-07-15 10:10:03 +0200

    properly implement write_conversions unit testing

--- test/test_write_conversions.cpp
+++ test/test_write_conversions.cpp
@@ -1,3 +1,4 @@
+#include <google/protobuf/text_format.h>
 #include <gtest/gtest.h>
 #include <string>
 
@@ -18,16 +19,105 @@ TEST(WriteConversionTests, TestStringMsg)
 
     conversions::packWriteItem(grpc_object, msg_to_test);
 
-    std::string unpacked_data;
-    std::string unpacked_instance_path;
+    std::string debugstr;
+    google::protobuf::TextFormat::PrintToString(*grpc_object, &debugstr);
 
-    /// @todo: Insert way to unpack the grpc_object to get unpacked_data and unpackeed_instance_path
-    // For now, dummy to pass test
-    unpacked_data = test_data;
-    unpacked_instance_path = test_instance_path;
+    ASSERT_TRUE(debugstr.find("PortName: \"test_path\"") !=std::string::npos);
+    /// Counted by first manually verifying debugstr
+    ASSERT_EQ(static_cast<int>(debugstr.find("StringValue: \"test_msg\"")), 121); // string.data
+}
 
-    EXPECT_EQ(test_data, unpacked_data);
-    EXPECT_EQ(test_instance_path, unpacked_instance_path);
+
+TEST(WriteConversionTests, TestTwistMsg)
+{
+  std::string test_instance_path = "test_path";
+
+  geometry_msgs::msg::Twist msg_to_test;
+  msg_to_test.linear.x = 1.11;
+  msg_to_test.linear.y = 2.22;
+  msg_to_test.linear.z = 3.33;
+  msg_to_test.angular.x = 4.44;
+  msg_to_test.angular.y = 5.55;
+  msg_to_test.angular.z = 6.66;
+
+  IDataAccessServiceWriteRequest request;
+  ::Arp::Plc::Gds::Services::Grpc::WriteItem * grpc_object = request.add_data();
+  grpc_object->set_portname(test_instance_path);
+  grpc_object->mutable_value()->set_typecode(::Arp::Type::Grpc::CoreType::CT_Struct);
+
+  conversions::packWriteItem(grpc_object, msg_to_test);
+
+  std::string debugstr;
+  google::protobuf::TextFormat::PrintToString(*grpc_object, &debugstr);
+
+  ASSERT_TRUE(debugstr.find("PortName: \"test_path\"") !=std::string::npos);
+  /// Counted by first manually verifying debugstr
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 1.11")), 200);  // linear.x
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 2.22")), 293);  // linear.y
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 3.33")), 386);  // linear.z
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 4.44")), 560);  // angular.x
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 5.55")), 653);  // angular.y
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 6.66")), 746);  // angular.z
+}
+
+
+TEST(WriteConversionTests, TestOdomMsg)
+{
+  std::string test_instance_path = "test_path";
+
+  nav_msgs::msg::Odometry msg_to_test;
+  msg_to_test.header.stamp.sec = 111;
+  msg_to_test.header.stamp.nanosec = 222;
+  msg_to_test.header.frame_id = "header_frame_id";
+  msg_to_test.child_frame_id = "child_frame_id";
+  msg_to_test.pose.pose.position.x = 1.11;
+  msg_to_test.pose.pose.position.y = 2.22;
+  msg_to_test.pose.pose.position.z = 3.33;
+  msg_to_test.pose.pose.orientation.x = 4.44;
+  msg_to_test.pose.pose.orientation.y = 5.55;
+  msg_to_test.pose.pose.orientation.z = 6.66;
+  msg_to_test.pose.pose.orientation.w = 7.77;
+  msg_to_test.pose.covariance.at(5) = 8.88;
+  msg_to_test.twist.twist.linear.x = 9.99;
+  msg_to_test.twist.twist.linear.y = 10.1010;
+  msg_to_test.twist.twist.linear.z = 11.1111;
+  msg_to_test.twist.twist.angular.x = 12.1212;
+  msg_to_test.twist.twist.angular.y = 13.1313;
+  msg_to_test.twist.twist.angular.z = 14.1414;
+  msg_to_test.twist.covariance.at(7) = 15.1515;
+
+  IDataAccessServiceWriteRequest request;
+  ::Arp::Plc::Gds::Services::Grpc::WriteItem * grpc_object = request.add_data();
+  grpc_object->set_portname(test_instance_path);
+  grpc_object->mutable_value()->set_typecode(::Arp::Type::Grpc::CoreType::CT_Struct);
+
+  conversions::packWriteItem(grpc_object, msg_to_test);
+
+  std::string debugstr;
+  google::protobuf::TextFormat::PrintToString(*grpc_object, &debugstr);
+
+  ASSERT_TRUE(debugstr.find("PortName: \"test_path\"") !=std::string::npos);
+  /// Counted by first manually verifying debugstr
+  ASSERT_EQ(static_cast<int>(debugstr.find("Int32Value: 111")) ,290);
+  ASSERT_EQ(static_cast<int>(debugstr.find("Uint32Value: 222")) , 397);
+  ASSERT_EQ(static_cast<int>(debugstr.find("StringValue: \"header_frame_id\"")), 515);
+  ASSERT_EQ(static_cast<int>(debugstr.find("StringValue: \"child_frame_id\"")), 623);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 1.11")),985);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 2.22")), 1110);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 3.33")), 1235);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 4.44")), 1481);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 5.55")), 1606);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 6.66")), 1731);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 7.77")), 1856);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 8.88")), 2622);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 9.99")), 6166);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 10.101")), 6291);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 11.11")), 6418);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 12.1212")), 6667);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 13.1313")), 6795);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 14.1414")), 6923);
+  ASSERT_EQ(static_cast<int>(debugstr.find("DoubleValue: 15.1515")), 7902);
+
 }
 
 int main(int argc, char ** argv)
