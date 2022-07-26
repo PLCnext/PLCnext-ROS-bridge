e2bbb79196b07ad8d95cf118d0b05e6c69c2223c
Tejas Kumar Shastha <tejas.kumar.shastha@ipa.fraunhofer.de>
2022-07-26 12:11:40 +0000

    update to succesfully test on PLC

--- tests/scripts/read_analog_io.py
+++ tests/scripts/read_analog_io.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
-
 import sys
 import rospy
 from phoenix_bridge.srv import AnalogIO, AnalogIORequest, AnalogIOResponse
 
-def service_call(val : bool):
+def service_call():
     rospy.wait_for_service('read_analog_io')
     try:
         set_client = rospy.ServiceProxy('read_analog_io', AnalogIO)
         req = AnalogIORequest()
         resp = AnalogIOResponse()
-        req.instance_path = "Arp.Plc.Eclr/MainInstance.ROS1_Obj.bool_data1"
+        req.instance_path = "Arp.Plc.Eclr/MainInstance.ROS1_Obj.analog_data"
         resp = set_client(req)
         print("Got response", resp.value)
         return resp.status
@@ -21,10 +19,7 @@ def service_call(val : bool):
 
 if __name__ == "__main__":
     rospy.init_node('service_caller', anonymous=True)
-    value = True
     rate = rospy.Rate(1) # 1Hz
     while not rospy.is_shutdown():
-        print("Calling single set with", value)
-        service_call(value)
-        value = not value
+        service_call()
         rate.sleep()
